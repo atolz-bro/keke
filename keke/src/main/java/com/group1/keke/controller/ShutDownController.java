c881d7d039c4c7edb3209266aa7a3a36282e93ae
Elijah <atolagbe.elijah.21@gmail.com>
2024-08-19 11:40:48 +0100

    shutdown controller

---
+++ keke/src/main/java/com/group1/keke/controller/ShutDownController.java
@@ -0,0 +1,24 @@
+package com.group1.keke.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class ShutDownController {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/shutdown")
+    void shutdown() throws IOException {
+        System.out.println("Shutdown Request");
+        //SpringApplication.exit(context, () -> 0);
+        //String command = "cd C:\\Users\\atola\\OneDrive\\Desktop\\Final Year Project\\Offline_Payment_System\\offlinepaymentsystem && .\\mvnw spring-boot:run";
+        //Process process = Runtime.getRuntime().exec(command);
+        //System.out.println("still innn.....");
+    }
+}
