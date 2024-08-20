6c2bec8230f825200a3cdf966dcfa1d435235003
Elijah <atolagbe.elijah.21@gmail.com>
2024-08-20 14:01:42 +0100

    shutdown controller edit

--- keke/src/main/java/com/group1/keke/controller/ShutDownController.java
+++ keke/src/main/java/com/group1/keke/controller/ShutDownController.java
@@ -1,6 +1,7 @@
 package com.group1.keke.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class ShutDownController {
     @PostMapping("/shutdown")
     void shutdown() throws IOException {
         System.out.println("Shutdown Request Hello Check Hook 1 2");
-        //SpringApplication.exit(context, () -> 0);
+        SpringApplication.exit(context, () -> 0);
         //String command = "cd C:\\Users\\atola\\OneDrive\\Desktop\\Final Year Project\\Offline_Payment_System\\offlinepaymentsystem && .\\mvnw spring-boot:run";
         //Process process = Runtime.getRuntime().exec(command);
         //System.out.println("still innn.....");
